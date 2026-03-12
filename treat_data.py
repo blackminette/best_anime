49fbd02d26290d711bf44742ce5400f905dd0519
Nicolas Vella <nicolasvella10@gmail.com>
2026-03-12 14:20:38 +0100

    discovery and start of treatment

--- treat_data.py
+++ treat_data.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 # Charger le dataset
 df = pd.read_csv("anime_dataset.csv")
+
+# Traiter les données
+print("NaN par colonne :")
+print(df.isnull().sum())
+print("\nDoublons :", df.duplicated().sum())
+
+df["score"] = df["score"].fillna(round(df["score"].mean(), 2))
+
+print(df.head(20))
+
+print(df.dtypes)
