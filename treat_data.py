3e4c49bd72b2b8b29ab2762e1e6cc073542429fe
Nicolas Vella <nicolasvella10@gmail.com>
2026-03-12 17:38:13 +0100

    data cleaning finalization

--- treat_data.py
+++ treat_data.py
@@ -4,16 +4,14 @@ import pandas as pd
 df = pd.read_csv("anime_dataset.csv")
 
 # Traiter les données
-print("NaN par colonne :")
-print(df.isnull().sum())
-print("\nDoublons :", df.duplicated().sum())
+df["episodes"] = df["episodes"].round().astype("Int64")
 
-# Remplissage des valeurs vide de la colone score par la moyenne de la colonne
-df["score"] = df["score"].fillna(round(df["score"].mean(), 2))
+# Suppression des données sans score
+df = df.dropna(subset="score")
 
-# Changement des types des colonnes episodes et year pour des Int64
-df[["episodes", "year"]] = df[["episodes", "year"]].astype("Int64")
+# Suppression des doublons
+df = df.drop_duplicates()
 
-print(df[["title", "score", "episodes"]].head(20))
+df["episodes"] = df["episodes"].fillna(0)
 
 print(df.dtypes)
