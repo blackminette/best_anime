4a2aa7483e2e1fb2587c44f03e63b6ff4e60caba
Nicolas Vella <nicolasvella10@gmail.com>
2026-03-12 14:01:40 +0100

    first commit

---
+++ get_dataset.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+import time
+
+base_url = "https://api.jikan.moe/v4"
+anime_data = []
+character_data = []
+
+max_pages = 2000
+
+for page in range(1, max_pages + 1):
+    print(f"Récupération de la page {page}")
+    r = requests.get(f"{base_url}/anime?page={page}&limit=25")
+    result = r.json()
+    
+    if "data" not in result or not result["data"]:
+        break
+
+    for anime in result["data"]:
+        aid = anime["mal_id"]
+        anime_data.append({
+            "anime_id": aid,
+            "title": anime["title"],
+            "score": anime["score"],
+            "type": anime["type"],
+            "episodes": anime["episodes"],
+            "year": anime.get("year"),
+            "genres": ", ".join([g["name"] for g in anime.get("genres", [])]),
+            "studios": ", ".join([s["name"] for s in anime.get("studios", [])])
+        })
+
+    time.sleep(1)  # éviter le rate limit
+
+print("Anime total récupérés :", len(anime_data))
+
+df = pd.DataFrame(anime_data)
+
+df.to_csv("anime_dataset.csv", index=False)
+
+print("Datasets enregistrés !")
